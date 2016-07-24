a5a4fe1c18814ca7a79075f48f3b315a0b6b8a2c
George Shen <george.school303@gmail.com>
2016-07-24 19:04:36 -0400

    Added area view

---
+++ buildingsViewModel.js
@@ -0,0 +1,55 @@
+//
+// Model object:
+// {
+// 		
+//		building1:
+//		{
+//			type: empty
+//		}
+//		building2:
+//		{
+//			type: wall
+//			tier: 1
+//		}
+//		building3:
+//		{
+//			type: wall
+//			tier: 1
+//		}
+//		building4:
+//		{
+//			type: wall
+//			tier: 1
+//		}
+//		building5:
+//		{
+//			type: wall
+//			tier: 1
+//		}
+//		building6:
+//		{
+//			type: wall
+//			tier: 1
+//		}
+//		building7:
+//		{
+//			type: wall
+//			tier: 1
+//		}
+//		building8:
+//		{
+//			type: wall
+//			tier: 1
+//		}
+//		building9:
+//		{
+//			type: wall
+//			tier: 1
+//		}
+// }
+//
+function initBuildingsViewModel(buildingsJq, model, coord) {
+}
+
+function cleanUp(buildingsJq) {
+}
